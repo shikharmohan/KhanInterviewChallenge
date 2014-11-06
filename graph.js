d11e9f23c372aab6beb9a9b4c161af7eab29dd3d
Shikhar Mohan <shikharmohan2016@u.northwestern.edu>
2014-11-06 00:04:34 -0600

    added component class to manage global users/coaches

--- graph.js
+++ graph.js
@@ -1,15 +1,21 @@
 $(document).ready(function(){
 	users = createSmallUserGraph();
 	console.log('Number of users: ');
-	$("#totalInfectionSubmit").on('submit', function(){
-		user_id = $("#startUser");
+
+	$("#totalInfectionSubmit").on('click', function(){
+		user_id = int($("#startUser"));
 		infect_user = findUser(user_id);
 		infectionID = Math.floor(Math.random() * 100);
 		total_infection = infect_user.total_infection(infectionID);
 		console.log('Infected Users:' + total_infection.length);
 	});
 
-
+	$("#limitedInfectionSubmit").on('click', function(){
+		user_id = int($("#startUserLimit").val());
+		limit = int($("#limit").val());
+		infect_user = findUser(user_id);
+		limited_infection = limitInfection(infect_user, limit);
+	});
 
 
 });
--- users.js
+++ users.js
@@ -1,10 +1,86 @@
-User = (function(){
+/*Creating a connected component object, basically a database. 
+* Makes creating d3 graph easier
+*/
+var Component = (function(){
+
+	function Component() {
+		this.users = [];
+		this.coaches = []; //creators of components, keep track
+	}
+
+	Component.prototype.createUsers = function(list) {
+		//assume list has only unique integers, otherwise we make list a set
+		for(var i = 0; i < list.length; i++){
+			user = new User(list[i])
+			this.addUser(user);
+		}
+	};
+
+	Component.prototype.addUser = function(user){
+		user.Component = this;
+		this.users.push(user);
+	};
+
+	Component.prototype.getUser = function(user_id){
+		users = this.users;
+		found_user = null;
+		for(var i = 0; i < users.length; i++){
+			if(users[i].id === user_id){
+				found_user = users[i];
+				break;
+			}
+		}
+		return found_user;
+	};
+	
+	Component.prototype.getUsers = function(list){
+		users = [];
+		for(var i = 0; i < list.length; i++){
+			user = this.getUser(list[i])
+			if(user !== null){
+				users.push(user);
+			}
+		}
+		return users;
+	};
+
+	Component.prototype.addCoach = function(){
+		if((this.coaches.indexOf(coach)) === -1){
+			return this.coaches.push(coach);
+		}
+	};
+
+	Component.prototype.numInfectedUsers = function(){
+		var count = 0;
+		users = this.users;
+		for(var i = 0; i < users.length; i++){
+			if(users[i].infected){
+				count++;
+			}
+		}
+		return count;
+	};
+
+
+	return Component;
+
+});
+
+
+
+
+
+
+
+
+var User = (function(){
 	function User(id){
 		this.id = id;
 		this.coaches = [];
 		this.students = [];
 		this.infected = false;
 		this.infectionID = 0;
+		this.Component = null;
 	}
 
 	User.prototype.addStudent = function(student) {
@@ -55,4 +131,24 @@ User = (function(){
 		}
 		return results;
 	};
+	User.prototype.limitedInfection = function(infectID) {
+		//infect current user & students if any
+		var count = 0;
+		if (if this.infectionID !== infectID){
+			this.infectionID = infectID;
+			if(!this.infected){
+				this.infect();
+				count++;
+			}
+			//infect students
+			std = this.students;
+			for(var i = 0; i < std.length; i++){
+				student = std[i];
+				count += student.limitedInfection(infectID);
+			}
+		}
+		return count;
+	};
+
+	return User;
 });
